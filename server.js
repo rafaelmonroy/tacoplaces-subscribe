df6701f8297f4f8669d6df4fc4617b64ba364633
rafaelmonroy <rafaelmonroy1990@gmail.com>
2019-09-17 22:25:21 -0700

    subscribe form working, server posts to mailchimp api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
@@ -11,7 +12,36 @@ app.get('/', (req, res) => {
 
 app.post('/', function(req, res) {
   var email = req.body.email;
+
   console.log(email);
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed'
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  fetch('https://us4.api.mailchimp.com/3.0/lists/885dd23f5b', {
+    method: 'POST',
+    headers: {
+      Authorization: 'rafael ea777093a4468ed3ce79ffb037c11dd6-us4'
+    },
+    body: jsonData
+  })
+    .then(e => {
+      console.log(e.status);
+    })
+    .catch(() => {
+      console.log('fetch error');
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+//list 885dd23f5b
+//api ea777093a4468ed3ce79ffb037c11dd6-us4
