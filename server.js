ad2f9b2c4b9eb8a8a60a4fe2179d10e2a9d5561d
rafaelmonroy <rafaelmonroy1990@gmail.com>
2019-09-18 19:20:51 -0700

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const config = require('./config');
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 const secretKey = config.SECRET_KEY;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('get request working');
-});
+//serve static assets if inproduction
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.post('/', (req, res) => {
   var email = req.body.email;
-
-  console.log(email);
 
   var data = {
     members: [
@@ -36,7 +41,6 @@ app.post('/', (req, res) => {
     body: jsonData
   })
     .then(e => {
-      console.log(e.status);
       if (e.status === 200) {
         res.redirect('/thankyou');
       }
